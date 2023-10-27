250ceca29432a8364252869dcb4f08541bfcc5a5
Marlon Ribeiro <marlonribeirot@gmail.com>
2023-10-27 19:17:29 -0300

    Fix: Minor fixes

--- js/maps.js
+++ js/maps.js
@@ -1,74 +1,91 @@
- // Variável global para o mapa
- var map;
+// Variáveis globais
+var map;
+var marker; // Variável global para o marcador
 
- // Função para inicializar o mapa
- function initMap() {
-   var latLng = { lat: -23.550520, lng: -46.633308 }; // Coordenadas de São Paulo, por exemplo
-   map = new google.maps.Map(document.getElementById('map'), {
-     center: latLng,
-     zoom: 13 // Ajuste o nível de zoom conforme necessário
-   });
- }
+// Função para inicializar o mapa
+function initMap() {
+  var latLng = { lat: -23.550520, lng: -46.633308 }; // Coordenadas de São Paulo, por exemplo
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: latLng,
+    zoom: 13 // Ajuste o nível de zoom conforme necessário
+  });
+}
 
- // Função para obter informações de um CEP
- function fetchCepData() {
-   var cep = $('#cep').val();
-   if (!cep) {
-     alert('Por favor, insira um CEP válido.');
-     return;
-   }
+// Função para adicionar um marcador no mapa
+function addMarkerToMap(location) {
+  if (marker) {
+    marker.setMap(null); // Remova o marcador existente, se houver
+  }
 
-   var apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+  marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: 'Localização do CEP'
+  });
+}
 
-   $.ajax({
-     url: apiUrl,
-     type: 'GET',
-     success: function (data) {
-       var resultHtml = '';
+// Função para obter informações de um CEP
+function fetchCepData() {
+  var cep = $('#cep').val();
+  if (!cep) {
+    alert('Por favor, insira um CEP válido.');
+    return;
+  }
 
-       if (data.erro) {
+  var apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  $.ajax({
+    url: apiUrl,
+    type: 'GET',
+    success: function (data) {
+      var resultHtml = '';
+
+      if (data.erro) {
         resultHtml = "CEP não encontrado";
       } else {
         let addressInfo = `${data.cep}, ${data.localidade}-${data.uf}.`;
-        
+
         if (data.logradouro) {
           addressInfo += ` ${data.logradouro}`;
         }
-        
+
         if (data.bairro) {
           addressInfo += ` - ${data.bairro}.`;
         }
-      
+
         resultHtml = `
           <h4>Resultado:</h4>
           ${addressInfo}
         `;
-      
+
         // Chame a função para mostrar a localização no mapa após obter os resultados do CEP
-        showLocationOnMap(data.localidade, data.uf);
+        var location = data.localidade + ', ' + data.uf;
+        showLocationOnMap(location);
       }
-      
 
-       $('#cep').val(''); // Limpar o campo CEP após a busca
-       $('#cepResults').html(resultHtml);
-     },
-     error: function () {
-       $('#cepResults').html("Erro ao obter informações do CEP");
-     }
-   });
- }
+      $('#cep').val(''); // Limpar o campo CEP após a busca
+      $('#cepResults').html(resultHtml);
+    },
+    error: function () {
+      $('#cepResults').html("Erro ao obter informações do CEP");
+    }
+  });
+}
 
- // Função para mostrar a localização no mapa
- function showLocationOnMap(localidade, uf) {
-   var geocoder = new google.maps.Geocoder();
+// Função para mostrar a localização no mapa
+function showLocationOnMap(location) {
+  var geocoder = new google.maps.Geocoder();
 
-   geocoder.geocode({ address: localidade + ', ' + uf }, function (results, status) {
-     if (status === 'OK' && results[0] && map) {
-       map.setCenter(results[0].geometry.location);
-     } else {
-       // Trate o erro ou mostre uma mensagem ao usuário
-       console.error('Erro ao geocodificar o endereço');
-     }
-   });
- }
- 
+  geocoder.geocode({ address: location }, function (results, status) {
+    if (status === 'OK' && results[0] && map) {
+      var locationData = results[0].geometry.location;
+      map.setCenter(locationData);
+
+      // Adicione um marcador no mapa
+      addMarkerToMap(locationData);
+    } else {
+      // Trate o erro ou mostre uma mensagem ao usuário
+      console.error('Erro ao geocodificar o endereço');
+    }
+  });
+}
